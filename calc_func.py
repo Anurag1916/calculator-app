f0ec1e5432261439a42163283b5e7813bbc0e9e5
Anurag1916 <anurag.shrivastava370@gmail.com>
2024-05-04 14:19:48 +0530

    added feature for division

--- calc_func.py
+++ calc_func.py
@@ -3,3 +3,9 @@ def do_addition(a:int, b:int):
 
 def do_subtraction(a,b):
     return a-b
+
+def do_division(a,b):
+    try:
+        return a/b
+    except ZeroDivisionError as e:
+        return "cant devide by zero"
